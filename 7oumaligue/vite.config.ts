2811f827963b5dbb472344202149df8f66dabaf6
Amine Barka <aminebaraka116@gmail.com>
2025-08-03 01:57:34 +0100

    fix 6

--- 7oumaligue/vite.config.ts
+++ 7oumaligue/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    minify: 'terser',
+    minify: 'esbuild',
     cssCodeSplit: true,
     sourcemap: false,
     rollupOptions: {
